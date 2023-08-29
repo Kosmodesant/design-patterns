23958e81b549c938a95e48cdd2b7aff6e48486db
Nikita <element_90@bk.ru>
2023-08-29 22:01:32 +0300

    release 1.0

---
+++ 03/task1/task1/task1.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <fstream>
+
+class LogCommand {
+public:
+    virtual ~LogCommand() = default;
+    virtual void print(const std::string& message) = 0;
+};
+
+class ConsoleLogCommand : public LogCommand {
+public:
+    void print(const std::string& message) override {
+        std::cout << "Console Log: " << message << std::endl;
+    }
+};
+
+class FileLogCommand : public LogCommand {
+public:
+    FileLogCommand(const std::string& filePath) : filePath(filePath) {}
+
+    void print(const std::string& message) override {
+        std::ofstream fileStream(filePath, std::ios::app);
+        if (fileStream.is_open()) {
+            fileStream << "File Log: " << message << std::endl;
+        }
+    }
+
+private:
+    std::string filePath;
+};
+
+void executeLogCommand(LogCommand& command, const std::string& message) {
+    command.print(message);
+}
+
+int main() {
+    ConsoleLogCommand consoleLog;
+    FileLogCommand fileLog("log.txt");
+
+    std::string message = "message";
+
+    executeLogCommand(consoleLog, message);
+    executeLogCommand(fileLog, message);
+
+    return 0;
+}
